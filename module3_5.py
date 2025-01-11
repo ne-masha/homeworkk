f22a5e123b532a99d3131bc6efe8ca3b3134e0fd
ne-masha <mnevskaa056@gmail.com>
2025-01-12 00:15:22 +0300

    Update module3_5.py
    Актуальный!!

--- module3_5.py
+++ module3_5.py
@@ -1,4 +1,7 @@
 def get_multiplied_digits(number):
+    if number % 10 == 0:
+        number = number // 10
+        
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
@@ -9,5 +12,5 @@ def get_multiplied_digits(number):
 result = get_multiplied_digits(40203)
 print(result)
 
-result2 = get_multiplied_digits(40203)
+result2 = get_multiplied_digits(402030)
 print(result2)
